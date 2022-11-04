b2c5d4da20b0186ea0d6c3b44e2c968d59817ba0
sol <sadlthf@icloud.com>
2022-11-04 11:33:16 +0900

    #1 - 회원가입 api 구현
    *  `properties` 파일은 hierarchy를 지원하지 않는 반면, `yaml`은 지원해서 작성할 때 조금 더 편리하고 보기 좋다. 따라서 수정했음
    * jpa 설정을 했다. heroku가 지원하는 heroku postgressql을 이용해서 쉽게 연결 할 수 있었음 `spring.datasource` 이하는 heroku에서 정보를 가져왔다.
    * datasource 를 연결햇으니 `@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)` 설정을 제거했다.
    *

--- src/main/java/com/sol/sns/SnsApplication.java
+++ src/main/java/com/sol/sns/SnsApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) -> DB 연결 안 할 때
+@SpringBootApplication
 public class SnsApplication {
 
 	public static void main(String[] args) {
