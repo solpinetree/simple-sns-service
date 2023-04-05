272cff0f56ac87d56a6273819f3fa6a43eff9863
sol <sadlthf@icloud.com>
2023-04-05 23:42:30 +0900

    feat: Sse 도입 - AlarmService 구현
    * 알람을 보내고, connection을 만드는 alarmService 구현

---
+++ src/main/java/com/sol/sns/service/AlarmService.java
@@ -0,0 +1,53 @@
+package com.sol.sns.service;
+
+import com.sol.sns.exception.ErrorCode;
+import com.sol.sns.exception.SnsApplicationException;
+import com.sol.sns.repository.EmitterRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AlarmService {
+
+    private final static Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
+    private final static String ALARM_NAME = "alarm";
+    private final EmitterRepository emitterRepository;
+
+    public void send(Integer alarmId, Integer userId) {
+        emitterRepository.get(userId).ifPresentOrElse(sseEmitter -> {
+            try {
+                sseEmitter.send(SseEmitter.event()
+                        .id(alarmId.toString())
+                        .name(ALARM_NAME)
+                        .data("new alarm"));
+            } catch (IOException e) {
+                emitterRepository.delete(userId);
+                throw new SnsApplicationException(ErrorCode.ALARM_CONNECT_ERROR);
+            }
+        }, () -> log.info("No emitter found"));
+    }
+
+    public SseEmitter connectAlarm(Integer userId) {
+        SseEmitter sseEmitter = new SseEmitter(DEFAULT_TIMEOUT);
+        emitterRepository.save(userId, sseEmitter);
+        sseEmitter.onCompletion(() -> emitterRepository.delete(userId));
+        sseEmitter.onTimeout(() -> emitterRepository.delete(userId));
+
+        try {
+            sseEmitter.send(SseEmitter.event()
+                    .id("id")
+                    .name(ALARM_NAME)
+                    .data("connect completed"));
+        } catch (IOException exception) {
+            throw new SnsApplicationException((ErrorCode.ALARM_CONNECT_ERROR));
+        }
+
+        return sseEmitter;
+    }
+}
