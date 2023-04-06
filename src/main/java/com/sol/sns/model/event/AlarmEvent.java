46c9993d51e457366b32067ef449c3e9daebb67a
sol <sadlthf@icloud.com>
2023-04-06 17:11:07 +0900

    feat: Kafka 도입
    consumer, producer 와 message로 보낼 `AlarmEvent` 구현

---
+++ src/main/java/com/sol/sns/model/event/AlarmEvent.java
@@ -0,0 +1,16 @@
+package com.sol.sns.model.event;
+
+import com.sol.sns.model.AlarmArgs;
+import com.sol.sns.model.AlarmType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlarmEvent {
+    private Integer receiveUserId;
+    private AlarmType alarmType;
+    private AlarmArgs args;
+}
